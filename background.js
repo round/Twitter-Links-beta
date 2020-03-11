11c06b1389ba89cb9c6165c739e627f8ffd7ea69
Maxim Leyzerovich <contact@round.is>
2020-03-11 01:18:07 -0700

    Adjusting popup positioning

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 function openSearch(tab) {
-  window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'twitterextension', 'top=' + window.screenTop +', left=' + (window.screenLeft + tab.width - 500) + ', width=500, height=' + tab.height);
+
+  chrome.windows.getCurrent(function(currentWindow) {
+    window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'TwitterLinks', 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' + (currentWindow.left + tab.width - 500) + ', width=500, height=' + tab.height);
+
+  });
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
