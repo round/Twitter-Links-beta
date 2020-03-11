ce2e7792878a3d876e7272d065c1ec1cd013497e
Maxim Leyzerovich <contact@round.is>
2020-03-11 03:12:06 -0700

    Background.js cleanup

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
 function openSearch(tab) {
 
   chrome.windows.getCurrent(function(currentWindow) {
-
-  // if (typeof TwitterLinksPopup !== 'undefined') {
-  //   TwitterLinksPopup.close();
-  //   delete TwitterLinksPopup;
-  // } else {
-    //stick in some per-tab logic here
-    TwitterLinksPopup = window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'TwitterLinks-' + tab.id, 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' + (currentWindow.left + tab.width - 500) + ', width=500, height=' + tab.height);
-  // }
+    //add per-tab logic and toggle / focus logic
+    TwitterLinksPopup = window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'TwitterLinks-' + tab.id, 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' +
   });
 }
 
