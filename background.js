3afb4946eb879d6d76a9ec3e9c096cd2591063f5
Maxim Leyzerovich <contact@round.is>
2020-03-11 01:59:19 -0700

    Beginnings of toggle per-tab function

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 function openSearch(tab) {
 
   chrome.windows.getCurrent(function(currentWindow) {
-    window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'TwitterLinks', 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' + (currentWindow.left + tab.width - 500) + ', width=500, height=' + tab.height);
 
+  // if (typeof TwitterLinksPopup !== 'undefined') {
+  //   TwitterLinksPopup.close();
+  //   delete TwitterLinksPopup;
+  // } else {
+    //stick in some per-tab logic here
+    TwitterLinksPopup = window.open('http://twitter.com/search?f=live&q=' + tab.url.split('?')[0] + '%20filter%3Alinks', 'TwitterLinks-' + tab.id, 'top=' + (currentWindow.top + (currentWindow.height - tab.height)) + ', left=' + (currentWindow.left + tab.width - 500) + ', width=500, height=' + tab.height);
+  // }
   });
 }
 
