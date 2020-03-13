c4c8deb9bc79a44f453051d916b757449a078f85
Maxim Leyzerovich <contact@round.is>
2020-03-13 14:12:45 -0700

    Adding tab title override

--- content.js
+++ content.js
@@ -1,15 +1,17 @@
-// (function() {
-	// console.log('content.js loaded');
+var style = document.createElement('style');
+style.innerHTML = `
+header[role="banner"],
+div[data-testid="primaryColumn"] > div:first-child > div:first-child > div:first-child {
+		display: none !important;
+	}
+`;
+document.head.appendChild(style);
 
-	var style = document.createElement('style');
-	style.innerHTML = `
-	header[role="banner"],
-	div[data-testid="primaryColumn"] > div:first-child > div:first-child > div:first-child {
-			display: none !important;
-		}
-	`;
-	document.head.appendChild(style);
-// })();
+// document.title = 'Loading Twitter Links\u2026';
 
-
-// https://twitter.com/search?f=live&q=https://developer.chrome.com/extensions/match_patterns%20filter%3Alinks&TwitterLinks
+var extensionTitle = 'Twitter Links';
+window.setInterval(function() {
+    if (document.title !== extensionTitle) {
+      document.title = extensionTitle;
+    }
+}, 100);
