cb3d5e4a3d876bd36f8160a694a06bf28e21ed28
Maxim Leyzerovich <contact@round.is>
2020-03-17 12:23:40 -0700

    Leaving only Top & Latest for results

--- content.js
+++ content.js
@@ -3,7 +3,31 @@ style.innerHTML = `
 header[role="banner"],
 div[data-testid="primaryColumn"] > div:first-child > div:first-child > div:first-child {
 		display: none !important;
-	}
+}
+
+div[role="tablist"] > div {
+	display: none;
+}
+
+div[role="tablist"] > div:nth-child(1),
+div[role="tablist"] > div:nth-child(2) {
+	display: block;
+	width: 50%;
+}
+
+div[role="tablist"] > div a {
+	padding-bottom: 15px;
+}
+
+div[aria-label="Previous"],
+div[aria-label="Next"] {
+	display: none !important;
+}
+
+nav[role="navigation"] > div > div {
+	width: 100%;
+}
+
 `;
 document.head.appendChild(style);
 
