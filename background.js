33c2cb741ef5fde02c71ac6c88dbe90b9c630fc8
swyx <shawnthe1@gmail.com>
2022-05-29 01:37:38 +0800

    support chrome manifest v3

--- background.js
+++ background.js
@@ -1,28 +1,29 @@
 function openSearch(tab) {
-  chrome.windows.getCurrent(function(currentWindow) {
-    TwitterLinksPopup = window.open(
-      'http://twitter.com/search?q=' +
+  chrome.windows.getCurrent(function (currentWindow) {
+    TwitterLinksPopup = chrome.windows.create({
+      url: 'http://twitter.com/search?q=' +
         encodeURIComponent(tab.url) +
+        '&src=typed_query' +
         '&TwitterLinks',
-      'TwitterLinks-' + tab.id,
-      'top=' +
-        (currentWindow.top + (currentWindow.height - tab.height)) +
-        ', left=' +
-        (currentWindow.left + tab.width - 400) +
-        ', width=400, height=' +
-        tab.height
+      tabId: tab.id,
+      top: (currentWindow.top + (currentWindow.height - tab.height)),
+      left: (currentWindow.left + tab.width - 400),
+      width: 400,
+      height: tab.height,
+    }
     );
   });
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(function (tab) {
   openSearch(tab);
 });
 
 chrome.contextMenus.create({
+  id: 'twitter-links-search',
   title: 'Twitter links to this page\u2026'
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
   openSearch(tab);
 });
